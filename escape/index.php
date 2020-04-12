ddee22a829e728966cc1c0d628fc1ba9a51f1bf3
Laura Ortuño <laura.ortuno@trialpanel.com>
2020-04-12 22:00:24 +0200

    Cambia texto inicial

--- escape/index.php
+++ escape/index.php
@@ -27,8 +27,9 @@
 			<header class="special">
 				<h2>El caso</h2>
 				<p>Sigue las pistas y atrapa al asesino antes de que escape.</p>
-				<p>Este juego se puede hacer solo o en grupo, solo necesitas un ordenador (o móvil, aunque se ve mejor en PC) y un bloc de notas. Debería resolverse en menos de
-					una hora...</p>
+				<p>Este juego se puede hacer solo o en grupo, solo necesitas un ordenador (o móvil, aunque se ve mejor en PC) y un bloc de notas.
+					Debería resolverse en menos de
+					69 minutos, que corra el reloj...</p>
 			</header>
 			<div class="content">
 				<p>Sois Jane Peralta y Tony Santiago, dos detectives de la comisaría de Aragón siguiendo la pista del famoso criminal Murderchef.
