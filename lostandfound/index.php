d1ae910443dc269b378e81e618c9221ae4736131
Laura Ortuño <laura.ortuno@trialpanel.com>
2021-05-07 20:39:05 +0200

    Sección he encontrado al perro

---
+++ lostandfound/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<title>Encuentra mi perro</title>
+	<meta charset="UTF-8">
+	<meta name="author" content="lortuno">
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<link rel="stylesheet" href="assets/css/main.css"/>
+</head>
+<body class="is-preload">
+
+<!-- Header -->
+<header id="header">
+	<div class="inner">
+		<a href="#" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
+		<h1><strong>Soy Tiburón</strong>, un perrete muy bueno aunque con mucho carácter<br />
+			trátame con respeto y no me juntes con perros grandes<br />
+			¡No les aguanto!</h1>
+	</div>
+</header>
+
+<!-- Main -->
+<div id="main">
+
+	<!-- One -->
+	<section id="one">
+		<header class="major">
+			<h2>Si estás viendo esto...<br />
+				probablemente me he perdido.</h2>
+		</header>
+		<p>Para ponerte en contacto con mis dueñas escribe a este Whatsapp 617936462.</p>
+		<p>O a este otro número 666915893.</p>
+		<p>¡Pregunta por Laura o Celia e irán a recogerme!</p>
+		<ul class="actions">
+			<li><a href="#three" class="button">Contactar por mail</a></li>
+		</ul>
+	</section>
+
+	<!-- Two -->
+	<section id="two">
+		<h2>Este soy yo</h2>
+		<p>Me llamo Tiburón, soy un mestizo de chihuaha, y aunque parezco muy pequeñito ya soy algo mayor.</p>
+		<p>No me gustan mucho las galletas, prefiero la comida de perro, y estar en mi camita.</p>
+		<p>Soy algo tímido al principio, ¡soy adoptado! pero ahora sé que me quieren mucho.</p>
+		<p>Los perros grandes me dan miedo y les ladro para marcar territorio, y no me gusta que me toquen el hocico, las orejas ni las patas.
+		Si tienes en cuenta eso nos llevaremos bien hasta que me vengan a buscar :)</p>
+		<div class="row">
+			<article class="col-6 col-12-xsmall work-item">
+				<a href="images/fulls/1.jpeg" class="image fit thumb"><img src="images/thumbs/1.jpeg" alt="" /></a>
+				<h3>De pie</h3>
+				<p>Me gusta ponerme a dos patas, sobre todo para pedir comida de humano.</p>
+			</article>
+			<article class="col-6 col-12-xsmall work-item">
+				<a href="images/2.jpeg" class="image fit thumb"><img src="images/thumbs/2.jpeg" alt="" /></a>
+				<h3>En el baño</h3>
+				<p>No estoy acomplejado pero tengo que vigilar mi peso...</p>
+			</article>
+			<article class="col-6 col-12-xsmall work-item">
+				<a href="images/fulls/3.jpeg" class="image fit thumb"><img src="images/thumbs/3.jpeg" alt="" /></a>
+				<h3>Al sol</h3>
+				<p>Lo que más me gusta es tomar el sol al calorcete.</p>
+			</article>
+			<article class="col-6 col-12-xsmall work-item">
+				<a href="images/fulls/4.jpeg" class="image fit thumb"><img src="images/thumbs/4.jpeg" alt="" /></a>
+				<h3>Cama</h3>
+				<p>Solo me subo a la cama de humano si me dejan eh.</p>
+			</article>
+			<article class="col-6 col-12-xsmall work-item">
+				<a href="images/fulls/5.jpeg" class="image fit thumb"><img src="images/thumbs/5.jpeg" alt="" /></a>
+				<h3>Sofá</h3>
+				<p>Al sofá si que he aprendido a subir solito.</p>
+			</article>
+			<article class="col-6 col-12-xsmall work-item">
+				<a href="images/fulls/6.jpeg" class="image fit thumb"><img src="images/thumbs/6.jpeg" alt="" /></a>
+				<h3>En mi cama</h3>
+				<p>Aquí paso mi día feliz, y más con mis juguetes.</p>
+			</article>
+		</div>
+	</section>
+
+	<!-- Three -->
+	<section id="three">
+		<h2>Escríbenos</h2>
+		<p>Seguramente estemos muy preocupadas y una llamada sea lo más rápido, pero si por cualquier razón necesitas enviar un email.</p>
+		<ul>
+			<li>Escribe a laura.ortunolopez@outlook.es</li>
+			<li>O utiliza el <a href="http://lortuno.mipropia.com/#Contact">formulario de contacto</a> de esta web </li>
+		</ul>
+	</section>
+
+</div>
+
+<!-- Footer -->
+<footer id="footer">
+	<div class="copyright">
+		&copy; lortuno. All rights reserved. Thanks for: Images <a href="https://unsplash.com">Unsplash</a> Design <a href="http://html5up.net">Starta</a>
+	</div>
+</footer>
+
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.poptrox.min.js"></script>
+<script src="assets/js/browser.min.js"></script>
+<script src="assets/js/breakpoints.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
